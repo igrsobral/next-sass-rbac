28f0b31917a7c5ea58a04f6a113bb1b998cc911d
igor.Sobral <igor.sobral@edenred.com>
2024-04-28 01:10:37 -0300

    chore: add prettier & ESLint in monorepo

--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@sass/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
