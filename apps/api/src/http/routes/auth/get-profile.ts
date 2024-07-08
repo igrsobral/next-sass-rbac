05c00cd7e02c71914a05bfaa73e8d9d3d6e2981e
Igor Sobral <igor_1917@hotmail.com>
2024-07-07 23:23:09 -0300

    feat(auth): add get profile endpoint

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,53 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Get authenticated user profile',
+        response: {
+          404: z.object({
+            message: z.string(),
+          }),
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+
+      if (!user) {
+        throw new Error('User is not found')
+      }
+
+      return reply.send({
+        user,
+      })
+    },
+  )
+}
