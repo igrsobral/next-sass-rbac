32abe425a1752d64234ba5ecee08eea4b4ad4c51
Igor Sobral <igor_1917@hotmail.com>
2024-07-10 22:32:25 -0300

    feat(org): add route to create organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecovery } from './routes/auth/request-password-recovery'
 import { resetPassword } from './routes/auth/reset-password'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -63,6 +64,7 @@ app.register(getProfile)
 app.register(requestPasswordRecovery)
 app.register(resetPassword)
 app.register(authenticateWithGithub)
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
